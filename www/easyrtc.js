d257f0566088d3b180f2bee61ec14152ffec9dc3
Vasanth <vasanthv87@gmail.com>
2017-01-13 11:17:39 +0530

    Update easyrtc.js

--- www/easyrtc.js
+++ www/easyrtc.js
@@ -50,11 +50,11 @@ return {
   "badUserName": "Illegal username {0}",
   "localMediaError": "Error getting local media stream: {0}",
   "miscSignalError": "Miscellaneous error from signalling server. It may be ignorable.",
-  "noServer": "Unable to reach the EasyRTC signalling server.",
+  "noServer": "Unable to reach the signalling server.",
   "badsocket": "Socket.io connect event fired with bad websocket.",
   "icf": "Internal communications failure",
   "statsNotSupported":"call statistics not supported by this browser, try Chrome.",
-   "noWebrtcSupport":"Your browser doesn't appear to support WebRTC.",
+   "noWebrtcSupport":"Your browser doesn't appear to support WebRTC, try Chrome.",
    "gumFailed":"Failed to get access to local media. Error code was {0}.",
    "requireAudioOrVideo":"At least one of audio and video must be provided"   
 };
