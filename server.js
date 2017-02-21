eb8dd90c7e87bb336db4027479fd854a50d91dc5
Vasanth <vasanthv87@gmail.com>
2017-02-21 20:08:39 +0530

    Few changes

--- server.js
+++ server.js
@@ -33,17 +33,14 @@ redisClient = redis.createClient({url: config.REDIS_URL});
 
 app.post('/api', function(req,res){
     var room = Math.random().toString(36).substr(2, 10);
-    res.json({text: 'Here is your video chat room URL https://sayhello.li/'+room, link: 'https://sayhello.li/'+room});
+    res.json({text: '!here is your video chat room URL https://sayhello.li/'+room, link: 'https://sayhello.li/'+room});
 });
 app.get('/oauth/slack', function(req,res){
     var request = require('request');
 
     request.post({url:'https://slack.com/api/oauth.access', form: {client_id:'143163789926.143168001270', client_secret: '52e8ce1440161b6feab5c52f3e330fb9', code: req.query.code, redirect_uri: 'https://sayhello.li/oauth/slack'}}, function(err,httpResponse,body){
-        res.json({
-            err: err,
-            httpResponse: httpResponse,
-            body: body
-        });
+        if(err) res.json(err);
+        else res.redirect('/');
     });
 
 });
