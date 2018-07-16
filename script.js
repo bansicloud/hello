823b51ce5af4886be6be89a49bafdc332da6cd5a
Vasanth <vasanth.vedamuthu@imaginea.com>
2018-07-16 18:33:27 +0530

    Working in Chrome & firefox

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ const peerConfig = {
         {'urls': 'stun:stun.iptel.org:3478'}
     ]
 };
-let peerConnection, localStream;
+let peerConnection, signalChannel, localStream, signal, isPeer;
 const remoteMessage = {sdp: null, ice: []};
+const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 
 const onGetUserMediaSuccess = (stream) => document.getElementById('selfVideo').srcObject = stream;
 const errorHandler = (error) => console.error(error);
@@ -27,6 +28,14 @@ if(navigator.mediaDevices.getUserMedia) {
 } else {
     alert('Your browser does not support getUserMedia API');
 }
+const signalMsgHandler = async event => {
+    console.log(event);
+    signal = JSON.parse(event.data);
+    isPeer && peerConnection.addStream(localStream);
+    await setPeerDescription(signal);
+    await setPeerIceCandidates(signal);
+}
+const msgHandler = event => {};
 
 const setUpPeerConnection = () => {
     peerConnection = new RTCPeerConnection(peerConfig);
@@ -34,8 +43,20 @@ const setUpPeerConnection = () => {
         if(event.candidate != null) remoteMessage.ice.push(event.candidate);
         else {
             // Completed the gathering of ICE candidate.
-            console.log(remoteMessage);
-            console.log(JSON.stringify(remoteMessage));
+            if(signalChannel){
+                if(signalChannel.readyState === 'open'){
+                    signalChannel.send(JSON.stringify(remoteMessage));
+                    return;
+                }
+            }
+            const signalHash = LZString.compressToBase64(JSON.stringify(remoteMessage));
+            //TODO: create URL here
+            //TODO: set it as room URL
+            if(isPeer){
+                console.log(signalHash);                    
+            }else{
+                console.log(window.location.href+'#'+signalHash);
+            }
         }
     }
     peerConnection.ontrack = (event) => {
@@ -43,43 +64,97 @@ const setUpPeerConnection = () => {
         console.log(event.streams[0]);
         document.getElementById('callerVideo').srcObject = event.streams[0];
     };
-    peerConnection.addStream(localStream);
-    peerConnection.ondatachannel = errorHandler;
-    console.log(peerConnection);
+    peerConnection.ondatachannel = (event) => {
+        console.log(event);
+        const channel = event.channel;
+        if(channel.label == 'signal'){
+            signalChannel = event.channel;
+            signalChannel.onmessage = signalMsgHandler;
+        }else if(channel.label == 'messages'){
+            msgChannel = event.channel;
+            msgChannel.onmessage = msgHandler;
+        }
+    };
+}
+const addStream = (stream) => peerConnection.addStream(stream);
+const createOffer = async () => {
+    const description = await peerConnection.createOffer({offerToReceiveAudio: 1});
+    await setLocalDescription(description);
 }
 
-const createOffer = () => {
-    peerConnection.createOffer().then(setLocalDescription).catch(errorHandler);
-    console.log(peerConnection);
+const createDataChannel = (name, msgHandler) => {
+    if(name === "signal"){
+        signalChannel = peerConnection.createDataChannel(name); 
+        signalChannel.onmessage = msgHandler;
+    }else if (name === "messages"){
+        msgChannel = peerConnection.createDataChannel(name);
+        msgChannel.onmessage = msgHandler;
+    }
 }
+const sendMsgDataChannel = (channel, msg) => channel.send(msg);
 
-const createDataChannel = () => {
-    const sendChannel = peerConnection.createDataChannel("sendChannel");
-    sendChannel.onopen = errorHandler;
-    sendChannel.onclose = errorHandler;
-    sendChannel.send('Hello');
-}
-
-const setLocalDescription = (description) => {
-    console.log('got description');
-    // Got local description
-    peerConnection.setLocalDescription(description).then(function() {
-      remoteMessage.sdp = peerConnection.localDescription;
-    }).catch(errorHandler);
+const setLocalDescription = async (description) => {
+    await peerConnection.setLocalDescription(description);
+    remoteMessage.sdp = peerConnection.localDescription;
 };
 
+const setPeerDescription = async (signal) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp));
+    // Only create answers in response to offers
+    console.log(peerConnection);
+    if(signal.sdp.type == 'offer') {
+        const description = await peerConnection.createAnswer();
+        isSafari && setTimeout(() => peerConnection.onicecandidate({candidate: null}), 1000);
+        await setLocalDescription(description);
+    }
+};
+
+const setPeerIceCandidates = async (signal) => {
+    const addIceCandidatePromises = signal.ice.map(ice => peerConnection.addIceCandidate(new RTCIceCandidate(ice)));
+    await Promise.all(addIceCandidatePromises);
+}
+
+const onPeerSignal = async (signalMsg) => {
+    signal = JSON.parse(LZString.decompressFromBase64(signalMsg));
+    await setPeerDescription(signal);
+    await setPeerIceCandidates(signal);
+    peerConnection.addStream(localStream);
+    // createDataChannel('messages', msgHandler);
+    await createOffer();
+    isSafari && setTimeout(() => peerConnection.onicecandidate({candidate: null}), 1000);
+}
+
+const startApp = async () => {
+    peerConnection || setUpPeerConnection();
+
+    if(!isPeer){
+        createDataChannel('signal', signalMsgHandler);
+        await createOffer();
+        isSafari && setTimeout(() => peerConnection.onicecandidate({candidate: null}), 1000);
+    }else{
+        await setPeerDescription(signal);
+        await setPeerIceCandidates(signal);
+    }
+}
 
 /* 
 
 peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
     // Only create answers in response to offers
     if(signal.sdp.type == 'offer') {
-      peerConnection.createAnswer().then(setLocalDescription).catch(errorHandler);
+        peerConnection.createAnswer().then(setLocalDescription).catch(errorHandler);
     }
-  }).catch(errorHandler);
+}).catch(errorHandler);
 
 
 
 signal.ice.forEach(ice => peerConnection.addIceCandidate(new RTCIceCandidate(ice)).then(errorHandler).catch(errorHandler))
 
 */
+
+// if the location.hash is present, treat as peer
+if(window.location.hash.length > 1){
+    signal = JSON.parse(LZString.decompressFromBase64(window.location.hash.substring(1)));
+    isPeer = true;
+}
+startApp();
